50b9c63aad64efd81958ed1c73d8faf23c493198
Alessandro Soares <alessandro.soares1604@gmail.com>
2025-12-30 04:15:06 -0300

    feat: adding ScalarConverter class interface

---
+++ ex00/includes/ScalarConverter.hpp
@@ -0,0 +1,23 @@
+#ifndef SCALARCONVERTER_HPP
+#define SCALARCONVERTER_HPP
+
+#include <string>
+
+class ScalarConverter {
+    public:
+        ScalarConverter();
+        ~ScalarConverter();
+        ScalarConverter(const ScalarConverter &copy);
+        ScalarConverter &operator=(const ScalarConverter &copy);
+
+        /*
+        toChar
+        toInt
+        toFloat
+        toDouble
+        */
+
+        static void convert(std::string &cppLiteral);
+};
+
+#endif
