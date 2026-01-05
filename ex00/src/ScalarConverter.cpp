f99d638252728a96328bb56690619ca2625c4ecb
aneri-da <alesssandro.soares1604@gmail.com>
2026-01-05 19:50:21 -0300

    feat:
    - adding the functions isChar, isInt, isFloat, isDouble to verify the types
    - adding the functions toChar, toInt, toFloat, toDouble to convert the input to the correct type
    - adding fromChar, fromInt, fromFloat, fromDouble to print the messages for the types
    - adding parser to validate the input
    - adding printLiterals to print all literals

--- ex00/src/ScalarConverter.cpp
+++ ex00/src/ScalarConverter.cpp
@@ -21,60 +21,188 @@ ScalarConverter &ScalarConverter::operator=(const ScalarConverter &copy) {
     return *this;
 }
 
-std::string toChar(std::string &str) {
-    if (str.length() == 1) {
-        if (!isascii(str[0])) {
-            return "impossible";
-        }
-        if (std::isprint(str[0])) {
-    
-            return "'" + str + "'";
-        }
-        else {
-            return "non displayble";
-        }
-    }
-    return "impossible";
+bool isChar(const std::string &str) {
+    return (str.length() == 1 && !std::isdigit(str[0]));
 }
 
-float toFloat(std::string &str) {
+bool isInt(const std::string &str) {
+    if (str.empty()) {
+        return false;
+    }
+    size_t i = (str[0] == '-' || str[0] == '+');
+    size_t len = str.length();
+    if (i == len) {
+        return false;
+    }
+    for (; i < len; i++) {
+        if (!std::isdigit(str[i]))
+            return false;
+    }
+    return true;
+}
+
+bool isFloat(const std::string &str) {
+    if (str.empty()) {
+        return false;
+    }
+    bool hasDot = false;
+    bool hasF = false;
+    size_t i = (str[0] == '-' || str[0] == '+');
+    size_t len = str.length();
+    if (i == len) {
+        return false;
+    }
+    if (str == "nanf" || str == "-inff" || str == "+inff") {
+        return true;
+    }  
+    for (; i < len; i++) {
+        if (str[i] == '.') {
+            hasDot = true;
+        }
+        else if (str[i] == 'f') {
+            hasF = true;
+        }
+        else if (!std::isdigit(str[i])) {
+            return false;
+        }
+    }
+    if (!hasDot || !hasF) {
+        return false;
+    }
+    return true;
+}
+
+bool isDouble(const std::string &str) {
+    if (str.empty()) {
+        return false;
+    }
+    bool hasDot = false;
+    size_t i = (str[0] == '-' || str[0] == '+');
+    size_t len = str.length();
+    if (i == len) {
+        return false;
+    }
+    if (str == "nan" || str == "-inf" || str == "+inf") {
+        return true;
+    }
+    for (; i < len; i++) {
+        if (str[i] == '.') {
+            hasDot = true;
+        }
+        else if (!std::isdigit(str[i])) {
+            return false;
+        }
+    }
+    if (!hasDot) {
+        return false;
+    }
+    return true;
+}
+
+int parser(std::string &cppLiteral) {
+    if (isChar(cppLiteral)) {
+        return CHAR;
+    }
+    else if (isInt(cppLiteral)) {
+        return INT;
+    }
+    else if (isFloat(cppLiteral)) {
+        return FLOAT;
+    }
+    else if (isDouble(cppLiteral)) {
+        return DOUBLE;
+    }
+    return UNKNOWN;
+}
+
+char toChar(const std::string &str) {
+    return str[0];
+}
+
+float toFloat(const std::string &str) {
     char *end;
     //const char *specialFloatCases[] = {"+inff", "-inff", "nanf"};
     float f = std::strtof(str.c_str(), &end);
-    if(std::isnan(f)) {
+    // if(std::isnan(f)) {
 
-    }
-    else if (std::isinf(f)) {
-    }
+    // }
+    // else if (std::isinf(f)) {
+    // }
     return f;
 }
 
-double toDouble(std::string &str) {
+double toDouble(const std::string &str) {
     char *end;
     //const char *specialDoubleCases[] = {"+inf", "-inf", "nan"};
 
     double d = std::strtod(str.c_str(), &end);
-    if(std::isnan(d)) {
+    // if(std::isnan(d)) {
 
-    }
-    else if (std::isinf(d)) {
-    }
+    // }
+    // else if (std::isinf(d)) {
+    // }
     return d;
 }
 
-int toInt(std::string &str) {
-    long i = std::atol(str.c_str());
-    if (i < INT_MIN || i > INT_MAX) {
-        //impossible
+long toLong(const std::string &str) {
+    long l = std::atol(str.c_str());
+    return l;
+}
+
+void fromChar(const std::string &str) {
+    char c = toChar(str);
+
+    std::cout << "char: ";
+    if (!isascii(c)) {
+        std::cout << "impossible" << std::endl;
     }
-    return (int)i;
+    else if (!std::isprint(c)) {
+        std::cout << "non displayble" << std::endl;
+    }
+    else {
+        std::cout << "'" << c << "'" << std::endl;
+    }
+}
+
+void fromInt(const std::string &str) {
+    std::cout << "int: ";
+    std::cout << toLong(str) << std::endl;
+}
+
+void fromFloat(const std::string &str) {
+    std::cout << "float: ";
+    std::cout << std::setprecision(1) << std::fixed << toFloat(str) << "f" << std::endl;
+}
+
+void fromDouble(const std::string &str) {
+    std::cout << "double: ";
+    std::cout << std::setprecision(1) << std::fixed << toDouble(str) << std::endl;
+}
+
+void printLiterals(const std::string &cppLiteral, int type) {
+    (void)type;
+    fromChar(cppLiteral);
+    fromInt(cppLiteral);
+    fromFloat(cppLiteral);
+    fromDouble(cppLiteral);
 }
 
 void ScalarConverter::convert(std::string &cppLiteral) {
-    if (cppLiteral.empty())
-        return;
-    std::cout << "char: " << toChar(cppLiteral) << std::endl;
-    std::cout << "int: " << toInt(cppLiteral) << std::endl;
-    std::cout << "float: " << std::setprecision(1) << std::fixed << toFloat(cppLiteral) << "f" << std::endl;
-    std::cout << "double: " << toDouble(cppLiteral) << std::endl;
+    int type = parser(cppLiteral);
+    if (type == CHAR) {
+        std::cout << "The parameter is a char literal" << std::endl;
+    }
+    else if (type == INT) {
+        std::cout << "The parameter is an int literal" << std::endl;
+    }
+    else if (type == FLOAT) {
+        std::cout << "The parameter is a float literal" << std::endl;
+    }
+    else if (type == DOUBLE) {
+        std::cout << "The parameter is a double literal" << std::endl;
+    }
+    else {
+        std::cout << "The parameter is not one of the types accepted" << std::endl;
+    }
+    printLiterals(cppLiteral, type);
 }
