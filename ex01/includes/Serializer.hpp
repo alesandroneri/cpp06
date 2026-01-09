eb452a27d981a5e09505ae1f59084f7e3024c48a
aneri-da <alesssandro.soares1604@gmail.com>
2026-01-09 19:25:35 -0300

    feat: adding canonical orthodox form in the interface of the class Serializer and adding the implementation of the canonical orthodox form of the class Serializer

--- ex01/includes/Serializer.hpp
+++ ex01/includes/Serializer.hpp
@@ -5,6 +5,12 @@
 #include <stdint.h>
 
 class Serializer {
+    private:
+        Serializer();
+        ~Serializer();
+        Serializer(const Serializer &copy);
+        Serializer &operator=(const Serializer &copy);
+
     public:
         static uintptr_t serialize(Data* ptr);
         static Data* deserialize(uintptr_t raw);
--- ex01/src/Serializer.cpp
+++ ex01/src/Serializer.cpp
@@ -1,5 +1,18 @@
 #include "../includes/Serializer.hpp"
 
+Serializer::Serializer(){}
+Serializer::~Serializer(){}
+Serializer::Serializer(const Serializer &copy) {
+    *this = copy;
+}
+
+Serializer &Serializer::operator=(const Serializer &copy) {
+    if (this != &copy) {
+        *this = copy;
+    }
+    return *this;
+}
+
 uintptr_t Serializer::serialize(Data *ptr) {
     return reinterpret_cast<uintptr_t>(ptr);
 }
