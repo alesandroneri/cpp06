30a78048cd8dc6036dd7b2403e75ea79c564aaa2
aneri-da <alesssandro.soares1604@gmail.com>
2026-01-21 14:16:31 -0300

    fix: removing forbidden function in the function identify with reference and using try catch to get the wrong casts

--- ex02/src/Base.cpp
+++ ex02/src/Base.cpp
@@ -5,7 +5,6 @@
 #include <cstdlib>
 #include <string>
 #include <iostream>
-#include <typeinfo>
 
 Base::~Base(){}
 
@@ -30,27 +29,40 @@ Base * generate(void) {
     }
     return b;
 }
-void identify(Base* p) {
+void identify(Base *p) {
     if (p == NULL) {
         std::cout << "Pointer is NULL" << std::endl;
         return;
     }
-    if (dynamic_cast<A*>(p) != NULL) {
+    if (dynamic_cast<A *>(p) != NULL) {
         std::cout << "A" << std::endl;
     }
-    else if (dynamic_cast<B*>(p) != NULL) {
+    else if (dynamic_cast<B *>(p) != NULL) {
         std::cout << "B" << std::endl;
     }
-    else if (dynamic_cast<C*>(p) != NULL) {
+    else if (dynamic_cast<C *>(p) != NULL) {
         std::cout << "C" << std::endl;
     }
 }
 
-void identify(Base& p) {
-    if (typeid(p) == typeid(A))
+void identify(Base &p) {
+
+    try {
+        static_cast<void>(dynamic_cast<A &>(p));
         std::cout << "A" << std::endl;
-    else if (typeid(p) == typeid(B))
+        return;
+    } catch (const std::bad_cast &wrongCast) {
+    }
+    try {
+        static_cast<void>(dynamic_cast<B &>(p));
         std::cout << "B" << std::endl;
-    else if (typeid(p) == typeid(C))
+        return;
+    } catch (const std::bad_cast &wrongCast) {
+    }
+    try {
+        static_cast<void>(dynamic_cast<C &>(p));
         std::cout << "C" << std::endl;
+        return;
+    } catch (const std::bad_cast &wrongCast) {
+    }
 }
